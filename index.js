32dcaf34a876e27b0483f6aecc28fc9d44f29d73
EmanAlzariey <eman_alzariey@yahoo.com>
2019-10-17 09:18:47 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,43 @@
+var routine = new Array(wakeDog , leashDog , walkToPark , throwFrisbee , walkHome ,unleashDog)
+
+function wakeDog(dogName, dogBreed) {
+    console.log(`Wake ${dogName} the ${dogBreed}`);
+    return `Wake ${dogName} the ${dogBreed}`; //return value
+  }
+
+  function leashDog(dogName, dogBreed) {
+    console.log(`Leash ${dogName} the ${dogBreed}`);
+    return `Leash ${dogName} the ${dogBreed}`;
+  }
+
+  function walkToPark(dogName, dogBreed) {
+    console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+    return `Walk to the park with ${dogName} the ${dogBreed}`;
+  }
+
+  function throwFrisbee(dogName, dogBreed) {
+    console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+    return `Throw the frisbee for ${dogName} the ${dogBreed}`;
+  }
+
+  function walkHome(dogName, dogBreed) {
+    console.log(`Walk home with ${dogName} the ${dogBreed}`);
+    return `Walk home with ${dogName} the ${dogBreed}`;
+  }
+  function unleashDog(dogName, dogBreed) {
+    console.log(`Unleash ${dogName} the ${dogBreed}`);
+    return `Unleash ${dogName} the ${dogBreed}`;
+  }
+
+  //new function to go through all functions in array use .map()
+
+  function exerciseDog(dogName, dogBreed){
+      
+        return routine.map(dogFunction => dogFunction(dogName, dogBreed));
+     
+  }
+
+
+
+
+
