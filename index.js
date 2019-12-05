45704f65c71b4aa9c6ec038e59c1eb47cb46ab74
Sarah Alafif <sar1997yah@gmail.com>
2019-12-05 19:25:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ let routine=[wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog] ;
 function exerciseDog(dogName, dogBreed){
   let res[];
   for(let i=0;i<routine.length-1;i++){
-    
+    routine[i](dogName,dogBreed);
   }
 }
  
